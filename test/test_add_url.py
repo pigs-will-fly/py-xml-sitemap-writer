9d03893bb83ccea07718f909f3e7601948a47c85
macbre <maciej.brencz@gmail.com>
2024-11-22 22:15:00 +0000

    Add test/test_add_url.py

---
+++ test/test_add_url.py
@@ -0,0 +1,52 @@
+"""
+Tests a sitemap's add_url method
+
+Mocks away all I/O related functions, lets the test assert the XML tag content
+"""
+
+from typing import Optional
+
+from xml_sitemap_writer import XMLSitemap
+from . import DEFAULT_HOST
+
+
+class MockedXMLSitemap(XMLSitemap):
+    """
+    Mocked version of the XMLSitemap class that does not perform writes
+    """
+
+    def __init__(self, root_url: str):
+        super().__init__(path="/", root_url=root_url)
+
+        self._write_to_sitemap_buf: Optional[str] = None
+
+    def _add_sitemap(self):
+        """
+        Skip writing gzip files while testing
+        """
+
+    def write_to_sitemap(self, buf: str, indent: bool = True):
+        """
+        Keeps the buf passed here for testing
+        """
+        self._write_to_sitemap_buf = buf
+
+    @property
+    def recent_write_to_sitemap_buf(self) -> Optional[str]:
+        """
+        A helper for assertions
+        """
+        return self._write_to_sitemap_buf
+
+
+def test_add_basic_url():
+    """
+    Asserts that the call creates a proper simple <url> tag
+    """
+    sitemap = MockedXMLSitemap(root_url=DEFAULT_HOST)
+    sitemap.add_url("/page_1.html")
+
+    assert (
+        sitemap.recent_write_to_sitemap_buf
+        == f"<url><loc>{DEFAULT_HOST}/page_1.html</loc></url>"
+    )
